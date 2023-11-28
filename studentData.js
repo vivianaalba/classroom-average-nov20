eab28413f06ab4505036dc4481601d78d443f4a2
Viviana Alba <141361486+vivianaalba@users.noreply.github.com>
2023-11-28 14:15:49 -0800

    add pages

---
+++ studentData.js
@@ -0,0 +1,28 @@
+let students = [
+    {
+        id: 1,
+        name: "Alice",
+        address: "123 Main Street",
+        averageGrade: 85,
+        },
+        {
+        id: 2,
+        name: "Bob",
+        address: "456 Elm Street",
+        averageGrade: 78,
+        },
+        {
+        id: 3,
+        name: "Charlie",
+        address: "789 Oak Street",
+        averageGrade: 92,
+        },
+        {
+        id: 4,
+        name: "David",
+        address: "321 Pine Street",
+        averageGrade: 70,
+        },
+]
+
+export default students;
