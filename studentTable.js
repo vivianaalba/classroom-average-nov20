86399adabc94c34e681abac231abbe311f1fcfbc
Viviana Alba <141361486+vivianaalba@users.noreply.github.com>
2023-11-28 16:13:06 -0800

    edit code

--- studentTable.js
+++ studentTable.js
@@ -9,7 +9,7 @@ function renderStudentTable(table, data) {
         const newStudent = document.createElement('tr');
         newStudent.innerHTML = `<td>${data[i].id}</td><td>${data[i].name}</td><td>${data[i].address}</td><td>${data[i].averageGrade}</td>`;
         
-        // Append the new row to the specified table
+        // Append the new row to the table
         table.appendChild(newStudent);
     }
 }
@@ -19,3 +19,13 @@ let table = document.getElementById("student-table");
 
 // Call the function with the table element and data
 renderStudentTable(table, data);
+
+// use DOM manipulation to create an input field for the average
+// Implement an onchange event handler that calls a function named updateGrade when 
+// a student's average grade is modified. This function will update the student's grade
+// and recalculate the classroom average.
+
+function updateGrade(inputElement) {
+    const inputValue = inputElement.value;
+    console.log(inputValue);
+}
