f532bdd0c2e23069e68425baa8bfa42bbdeda7b6
viviana <vivianaalba7@gmail.com>
2023-11-29 11:40:18 -0800

    edit to studentTable.js

---
+++ calculateClassroomAverage.js
@@ -0,0 +1,17 @@
+import data from './studentData.js';
+
+// Function to calculate and display the classroom average
+function calculateClassroomAverage(data) {
+    const totalGrades = data.reduce((sum, student) => sum + student.averageGrade, 0);
+    const classroomAverage = totalGrades / data.length;
+
+    // Display the classroom average (you should have an element with id 'classroomAverage' in your HTML)
+    const classroomAverageElement = document.getElementById('classroomAverage');
+    if (classroomAverageElement) {
+        classroomAverageElement.textContent = `Classroom Average: ${classroomAverage.toFixed(2)}`;
+    }
+}
+
+calculateClassroomAverage(data);
+
+export default calculateClassroomAverage;
--- studentTable.js
+++ studentTable.js
@@ -2,14 +2,18 @@
 import data from './studentData.js';
 console.log(data);
 
+// Import Classroom Average Calculation to recalculate upon editing a student average
+import calculateClassroomAverage from './calculateClassroomAverage.js'
+
 // Create function that makes a table based on student data
 function renderStudentTable(table, data) {
     for (let i = 0; i < data.length; i++) {
-        // Create a new row element in the table
+        // Create a new row element in the table with each student's information
+      // average grade should be an input with a preset value of the student's initial average
         const newStudent = document.createElement('tr');
-        newStudent.innerHTML = `<td>${data[i].id}</td><td>${data[i].name}</td><td>${data[i].address}</td><td>${data[i].averageGrade}</td>`;
-        
-        // Append the new row to the specified table
+        newStudent.innerHTML = `<td>${data[i].id}</td><td>${data[i].name}</td><td>${data[i].address}</td><td><input type="number" id ="inputElement" value="${data[i].averageGrade}"></td>`;
+
+        // Append the new row to table
         table.appendChild(newStudent);
     }
 }
@@ -20,7 +24,26 @@ let table = document.getElementById("student-table");
 // Call the function with the table element and data
 renderStudentTable(table, data);
 
-// use DOM manipulation to create an input field for the average
-// Implement an onchange event handler that calls a function named updateGrade when 
-// a student's average grade is modified. This function will update the student's grade
-// and recalculate the classroom average.
+// implement event handler to recalculate class average when a student average has changed
+table.addEventListener("change", updateGrade)
+
+
+// Function to update a student's grade and recalculate the classroom average
+function updateGrade(event) {
+  const inputElement = event.target;
+  const inputValue = inputElement.value.trim();
+  const rowIndex = inputElement.closest('tr').rowIndex;
+
+  // Update the corresponding student's grade in the data array
+  data[rowIndex - 1].averageGrade = parseFloat(inputValue);
+
+  for (let i = 0; i < data.length; i++) {
+    // array that extracts student ID and grade
+    let studentData = [data[i].id, data[i].averageGrade];
+  }
+
+  calculateClassroomAverage(data)
+}
+
+// export
+export default updateGrade;
